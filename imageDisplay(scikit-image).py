5a3fed7c22960780b2cb5150561f54db17e25f1c
Obaid Ullah <obaidsafi31@gmail.com>
2024-11-21 20:57:50 +0500

    Sciket image

--- imageDisplay(scikit-image).py
+++ imageDisplay(scikit-image).py
@@ -1,1 +1,10 @@
 from skimage import io, transform
+
+from main import resized_image
+
+image = io.imread("004.jpg")
+
+resized_image = transform.resize(image, (256, 256))
+
+io.show(resized_image)
+io.show
